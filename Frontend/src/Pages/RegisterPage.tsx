712cc05f9edb1f62361ac16b29ab634af274c9a1
Bsradha <bsradhanjali005@gmail.com>
2025-11-08 11:56:00 +0530

    LoginPage Added

--- Frontend/src/Pages/RegisterPage.tsx
+++ Frontend/src/Pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { Button, PasswordInput, TextInput } from '@mantine/core'
+import { Button, PasswordInput, SegmentedControl, TextInput } from '@mantine/core'
 import { useForm } from '@mantine/form';
 import { IconHeartbeat } from '@tabler/icons-react'
 import { Link } from 'react-router-dom';
@@ -8,13 +8,16 @@ const RegisterPage = () => {
 
      const form = useForm({
         initialValues: {
+        type:"PATIENT",
         email: '',
         password:'',
+        confirmPassword:"",
     },
 
     validate: {
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      password:(value)=>(!value?"Password is required":null)
+      password:(value)=>(!value?"Password is required":null),
+      confirmPassword:(value, values) => (value === values.password?null : "Passwords don't match")
     },
   });
 
@@ -31,6 +34,7 @@ const RegisterPage = () => {
       <div className='w-[450px] backdrop-blur-md p-10 py-8 rounded-lg' >
         <form action=""  onSubmit={form.onSubmit(handleSubmit)} className='flex flex-col gap-5 [&_input]:placeholder-neutral-100! [&_.mantine-Input-input]:border-white!  [&_.mantine-Input-input]:border [&_input]:pl-2! [&_svg]:text-white [&_input]:text-white!'>
             <div className='self-center font-medium font-serif  text-white text-xl '>Register</div>
+            <SegmentedControl {...form.getInputProps("type")} fullWidth size="md" radius="md" color='pink'bg="none" className='**:text-white! border border-white' data={[{label:'Patient',value:"PATIENT"}, {label:'Doctor',value:"DOCTOR"}, {label:'Admin',value:"ADMIN"}]} />
                 <TextInput className='focus-within:[&_.mantine-Input-input]:border-pink-400! transition duration-300'
                     variant="unstyled"
                     size="md"
@@ -44,6 +48,13 @@ const RegisterPage = () => {
                     radius="md"
                     placeholder="Password"
                     {...form.getInputProps('password')}
+                />
+                <PasswordInput className='focus-within:[&_.mantine-Input-input]:border-pink-400! transition duration-300'
+                    variant="unstyled"
+                    size="md"
+                    radius="md"
+                    placeholder="confirmPassword"
+                    {...form.getInputProps('confirmPassword')}
                 />
                 <Button type='submit' color='pink' radius="md" size='md'>Register</Button>
                 <div className='text-neutral-100 text-sm self-c'>Have an account? <Link to="/login" className='hover:underline'>Login</Link></div>
